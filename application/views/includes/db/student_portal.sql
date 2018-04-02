9814a940eba179401b1f1f80ef12fba222e9ac77
shaun554 <shaundindial030@gmail.com>
2018-04-03 00:08:48 +0530

    Updated database script file

--- application/views/includes/db/student_portal.sql
+++ application/views/includes/db/student_portal.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 29, 2018 at 10:08 AM
+-- Generation Time: Apr 02, 2018 at 08:37 PM
 -- Server version: 10.1.16-MariaDB
 -- PHP Version: 7.0.9
 
@@ -32,8 +32,24 @@ CREATE TABLE `books` (
   `url` varchar(500) NOT NULL,
   `subject` varchar(100) NOT NULL,
   `author` varchar(200) NOT NULL,
-  `slug` varchar(500) NOT NULL
+  `slug` varchar(500) NOT NULL,
+  `tag` varchar(100) DEFAULT NULL,
+  `added_by` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `books`
+--
+
+INSERT INTO `books` (`id`, `name`, `url`, `subject`, `author`, `slug`, `tag`, `added_by`) VALUES
+(1, 'Ajax For Dummies', 'https://hmt.es/Ajax%20For%20Dummies.pdf', 'AJAX', 'Steven Holzner', '', '#web#', 1),
+(2, 'Software Engineering', 'https://google.com', 'Software Engineering', 'Alex Banks, Eve Porcello', '', 'Theory#UML', 2),
+(3, '.NET Book Zero', 'http://google.com', '.NET', 'NA', '', 'Web#Microsoft', 1),
+(4, 'Learning Python', 'http://google.com', 'Python', ' David Ascher and Mark Lutz', '', '', 1),
+(5, 'Data Communications and Networking', 'http://library.aceondo.net/ebooks/Computer_Science/Data_Communication_and_Networking_by_Behrouz.A.Forouzan_4th.edition.pdf', 'Data Communications and Networking', 'Behrouz A. Forouzan', '', 'Networking#Theory', 1),
+(6, 'Learning React: Functional Web Development with React and Redux', 'http://shop.oreilly.com/product/0636920049579.do', 'React Js', ' Alex Banks, Eve Porcello', '', 'Web', 2),
+(7, 'Android Application Development for Dummies', 'https://books.google.co.in/books/about/Android_App_Development_For_Dummies.html?id=UjqkBgAAQBAJ&printsec=frontcover&source=kp_read_button&redir_esc=y#v=onepage&q&f=false', 'Android', 'Donn Felker', '', 'Android', 2),
+(8, 'Learning Vue.js 2', 'https://books.google.co.in/books/about/Learning_Vue_js_2.html?id=nszcDgAAQBAJ&printsec=frontcover&source=kp_read_button&redir_esc=y#v=onepage&q&f=false', 'Web Development', 'Olga Filipova', '', 'Web', 2);
 
 --
 -- Triggers `books`
@@ -65,6 +81,15 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`) VALUES
+(1, 'Admin', 'jayashankar.jayan@gmail.com', '23d42f5f3f66498b2c8ff4c20b8c5ac826e47146', 'admin'),
+(2, 'Jayashankar', 'teacher@gmail.com', '23d42f5f3f66498b2c8ff4c20b8c5ac826e47146', 'teacher'),
+(3, 'Student', 'jayashankar.jayan@gmail.com', 'fe045f7c7a36e347c1723f52e02e077bc92f67ca', 'student');
+
+--
 -- Triggers `users`
 --
 DELIMITER $$
@@ -87,7 +112,8 @@ DELIMITER ;
 -- Indexes for table `books`
 --
 ALTER TABLE `books`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `added_by` (`added_by`);
 
 --
 -- Indexes for table `users`
@@ -103,12 +129,23 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `books`
 --
 ALTER TABLE `books`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `books`
+--
+ALTER TABLE `books`
+  ADD CONSTRAINT `books_ibfk_1` FOREIGN KEY (`added_by`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `books_ibfk_2` FOREIGN KEY (`added_by`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
